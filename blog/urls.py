7739b7e15eb7d7183e7f114fbadb8dc1ac3b4fb3
John Polar <jp@gmail.com>
2019-05-19 13:13:50 +0300

    first

--- blog/urls.py
+++ blog/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('about/', views.about, name='blog-about'),
     path('reaction', ReactionView.as_view(), name='reaction'),
     url(r'^post/addcomment/(?P<article_id>[0-9]+)',  views.addcomment, name='addcomment')
 ]
